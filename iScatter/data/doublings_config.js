cdff8ee959ead14d26fec3fc40e9db7826ca6021
Tony Hirst <tony.hirst@gmail.com>
2014-08-13 14:02:01 +0100

    Add doublings demo

---
+++ iScatter/data/doublings_config.js
@@ -0,0 +1,30 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/doublings.txt",
+    "C_DESC": "Doublings data",
+    "C_INTVARS": [],
+    "C_NAME": "Doublings",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/doublings_schema.txt",
+    "C_STATS": {
+        "x": [],
+        "y": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ]
+    },
+    "C_SUBSETS": [
+        "typ"
+    ],
+    "C_XATTR": [],
+    "C_XINIT": "x",
+    "C_XSCALE": "log",
+    "C_YATTR": [],
+    "C_YINIT": "y",
+    "C_YRANGE": "",
+    "C_YSCALE": ""
+}
