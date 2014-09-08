5d2db9a44737ff4a092cc93ceb484e9f8a3856d6
Tony Hirst <tony.hirst@gmail.com>
2014-09-08 15:07:24 +0100

    Grouped fertility, life expectancy, under 5 mortality

---
+++ iScatter/data/fertlife5mortGroup_config.js
@@ -0,0 +1,33 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/fertlife5mortGroup.txt",
+    "C_DESC": "Fertility rate (SP.DYN.TFRT.IN), life expectancy at birth(SP.DYN.LE00.IN) and\nunder 5 mortality (SH.DYN.MORT) for several countries by year. Source: World Bank Development Indicator ",
+    "C_INTVARS": [],
+    "C_NAME": "Fertility rate, Life expectancy and Under 5 Mortality at birth",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/fertlife5mortGroup_schema.txt",
+    "C_STATS": {
+        "SP.DYN.TFRT.IN": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ],
+        "year": []
+    },
+    "C_SUBSETS": [
+        "country"
+    ],
+    "C_XATTR": [],
+    "C_XINIT": "year",
+    "C_XRANGE": "",
+    "C_XSCALE": "",
+    "C_YATTR": [
+        "year"
+    ],
+    "C_YINIT": "SP.DYN.TFRT.IN",
+    "C_YRANGE": "",
+    "C_YSCALE": ""
+}
