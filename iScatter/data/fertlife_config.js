c4b6d3bb1d0dea77432723048ca5474dfd924d7b
Tony Hirst <tony.hirst@gmail.com>
2014-09-08 14:43:50 +0100

    Fetility and life expectancy data

---
+++ iScatter/data/fertlife_config.js
@@ -0,0 +1,34 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/fertlife.txt",
+    "C_DESC": "Fertility rate (SP.DYN.TFRT.IN) and life expectancy at birth(SP.DYN.LE00.IN) for several countries by year. Source: World Bank Development Indicator ",
+    "C_INTVARS": [],
+    "C_NAME": "Fertility rate and Life expectancy at birth",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/fertlife_schema.txt",
+    "C_STATS": {
+        "SP.DYN.TFRT.IN Bangladesh": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ],
+        "year": []
+    },
+    "C_SUBSETS": [],
+    "C_XATTR": [],
+    "C_XINIT": "year",
+    "C_XRANGE": "",
+    "C_XSCALE": "",
+    "C_YATTR": [
+        "year"
+    ],
+    "C_YINIT": "SP.DYN.TFRT.IN Bangladesh",
+    "C_YRANGE": [
+        0,
+        8
+    ],
+    "C_YSCALE": ""
+}
