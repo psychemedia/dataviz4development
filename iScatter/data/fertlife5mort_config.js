0761b3358bed2112e014eb66561db585b9089c0e
Tony Hirst <tony.hirst@gmail.com>
2014-09-08 14:58:15 +0100

    Life expectancy, fertility and under 5 mortality

---
+++ iScatter/data/fertlife5mort_config.js
@@ -0,0 +1,31 @@
+CONFIG={
+    "C_AINIT": "SP.DYN.TFRT.IN China",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/fertlife5mort.txt",
+    "C_DESC": "Fertility rate (SP.DYN.TFRT.IN), life expectancy at birth(SP.DYN.LE00.IN) and\nunder 5 mortality (SH.DYN.MORT) for several countries by year. Source: World Bank Development Indicator ",
+    "C_INTVARS": [],
+    "C_NAME": "Fertility rate, Life expectancy and Under 5 Mortality at birth",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/fertlife5mort_schema.txt",
+    "C_STATS": {
+        "SH.DYN.MORT China": [],
+        "SP.DYN.LE00.IN China": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ]
+    },
+    "C_SUBSETS": [],
+    "C_XATTR": [],
+    "C_XINIT": "SH.DYN.MORT China",
+    "C_XRANGE": "",
+    "C_XSCALE": "",
+    "C_YATTR": [
+        "year"
+    ],
+    "C_YINIT": "SP.DYN.LE00.IN China",
+    "C_YRANGE": "",
+    "C_YSCALE": ""
+}
