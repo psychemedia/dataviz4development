23462cea530760896b488d69813859ea750f6d43
Tony Hirst <tony.hirst@gmail.com>
2014-09-08 14:33:30 +0100

    Add simple fertility chart demo

---
+++ iScatter/data/fert_config.js
@@ -0,0 +1,34 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/fert.txt",
+    "C_DESC": "Fertility rate for several countries by year. Source: World Bank Development Indicator ",
+    "C_INTVARS": [],
+    "C_NAME": "Fertility rate (SP.DYN.TFRT.IN)",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/fert_schema.txt",
+    "C_STATS": {
+        "Bangladesh": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ],
+        "year": []
+    },
+    "C_SUBSETS": [],
+    "C_XATTR": [],
+    "C_XINIT": "year",
+    "C_XRANGE": "",
+    "C_XSCALE": "",
+    "C_YATTR": [
+        "year"
+    ],
+    "C_YINIT": "Bangladesh",
+    "C_YRANGE": [
+        0,
+        8
+    ],
+    "C_YSCALE": ""
+}
