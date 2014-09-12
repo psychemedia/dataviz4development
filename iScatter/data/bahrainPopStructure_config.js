0df62939b4857e41a5df8672501317fae93533d0
Tony Hirst <tony.hirst@gmail.com>
2014-09-12 11:46:33 +0100

    iScatter - Bahrain population structure fragment

---
+++ iScatter/data/bahrainPopStructure_config.js
@@ -0,0 +1,34 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/bahrainPopStructure.txt",
+    "C_DESC": "Bahrain Population Structure",
+    "C_INTVARS": [],
+    "C_NAME": "Bahrain Population Structure",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/bahrainPopStructure_schema.txt",
+    "C_STATS": {
+        "Value": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ],
+        "Year": []
+    },
+    "C_SUBSETS": [
+        "Age"
+    ],
+    "C_XATTR": [],
+    "C_XINIT": "Year",
+    "C_XRANGE": "",
+    "C_XSCALE": "",
+    "C_YATTR": [],
+    "C_YINIT": "Value",
+    "C_YRANGE": [
+        0,
+        200
+    ],
+    "C_YSCALE": ""
+}
