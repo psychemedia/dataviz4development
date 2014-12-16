a1e4d727592d7a5b5d139000b53be56e65e264f3
Tony Hirst <tony.hirst@gmail.com>
2014-12-16 19:35:20 +0000

    Fertility indicator line chart

---
+++ datawrapper/fertilityLine/js/default-b0b27d4baeaba05dbf63e06e31d632aa.min.js
@@ -0,0 +1,5 @@
+/*
+ * datawrapper / theme / default v1.5.1
+ * generated on 2014-05-21T09:57:00+02:00
+ */
+(function(){dw.theme.register('default',{colors:{palette:["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd"],secondary:["#000000",'#777777','#cccccc','#ffd500','#6FAA12'],context:'#aaa',axis:'#000000',positive:'#1f77b4',negative:'#d62728',background:'#ffffff'},lineChart:{fillOpacity:0.2},vpadding:10,frame:false,verticalGrid:false,columnChart:{darkenStroke:5}});}).call(this);
