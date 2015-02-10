71c5c79d257578aec41571355eeb2a6c52fbda62
Tony Hirst <tony.hirst@gmail.com>
2015-02-10 12:26:07 +0000

    Tweak dimensions (need to parametrise?)

--- populationCharts/scripts/d3_popPyramid_multi.js
+++ populationCharts/scripts/d3_popPyramid_multi.js
@@ -1,6 +1,8 @@
+var pwidth=600
+var pheight=400
 var margin = {top: 20, right: 40, bottom: 30, left: 20},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
+    width = pwidth - margin.left - margin.right,
+    height = pheight  - margin.top - margin.bottom,
     barWidth = Math.floor(width / 19) - 1;
 
 var x = d3.scale.linear()
