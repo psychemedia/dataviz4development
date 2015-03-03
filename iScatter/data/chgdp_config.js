e97b68111b14d72c25d4c7d21eadd759d31d75ee
Tony Hirst <tony.hirst@gmail.com>
2015-03-03 16:44:26 +0000

    China GDP

---
+++ iScatter/data/chgdp_config.js
@@ -0,0 +1,30 @@
+CONFIG={
+    "C_AINIT": "",
+    "C_CHARTID": "",
+    "C_COLOURGROUP": "",
+    "C_DATA": "./data/chgdp.txt",
+    "C_DESC": "Data from Gapminder",
+    "C_INTVARS": [],
+    "C_NAME": "China GDP",
+    "C_REFERENCES": {},
+    "C_SCHEMA": "./data/chgdp_schema.txt",
+    "C_STATS": {
+        "tot_gdp": [
+            "median",
+            "mean",
+            "stddev",
+            "range"
+        ],
+        "year": []
+    },
+    "C_SUBSETS": [],
+    "C_XATTR": [],
+    "C_XINIT": "year",
+    "C_XSCALE": "",
+    "C_YATTR": [
+        "year"
+    ],
+    "C_YINIT": "tot_gdp",
+    "C_YRANGE": "",
+    "C_YSCALE": "log"
+}
